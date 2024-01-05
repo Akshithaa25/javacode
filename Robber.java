49c4104729fa8d7dbaa98a52d15ac085faec90ee
Akshithaa25 <139947703+Akshithaa25@users.noreply.github.com>
2024-01-05 23:25:10 +0530

    Update Robber.java

--- Robber.java
+++ Robber.java
@@ -1,48 +1,106 @@
-from abc import ABC, abstractmethod
+import java.util.Scanner;
+abstract class Robber{
+void RobbingClass(){System.out.println("MScAi&ML");}
 
-# Abstract class Robber
-class Robber(ABC):
+abstract int RoundHouse(int[] xxx);
+abstract int RowHouse(int[] xxx);
+abstract int SquareHouse(int[] xxx);
+abstract int MultiHouse(int[] xxx);
+void MachineLearning(){System.out.println("I Love Machine Learning");}
+}
 
-    # Abstract method
-    @abstractmethod
-    def RobbingClass(self):
-        pass
-
-    # Default method
-    def MachineLearning(self):
-        print("I love MachineLearning.")
-
-# Class JAVAProfessionalRobber inheriting from Robber
-class JAVAProfessionalRobber(Robber):
-
-    # Implementing the abstract method from the base class
-    def RobbingClass(self):
-        print("MScAI&ML")
-
-    # Implementing additional abstract methods
-    def RowHouses(self):
-        print("Robbing row houses")
-
-    def RoundHouses(self):
-        print("Robbing round houses")
-
-    def SquareHouse(self):
-        print("Robbing square houses")
-
-    def RectangleHouse(self):
-        print("Robbing rectangle houses")
-
-# Example usage
-if __name__ == "__main__":
-    # Create an instance of the derived class
-    java_professional = JAVAProfessionalRobber()
-
-    # Call the implemented and inherited methods
-    java_professional.RobbingClass()
-    java_professional.RowHouses()
-    java_professional.RoundHouses()
-    java_professional.SquareHouse()
-    java_professional.RectangleHouse()
-
-    # Call the default method from the abstract class
-    java_professional.MachineLearning()
+class JAVAProfessionalRobber extends Robber{
+int maxrob=0;
+int RowHouse(int[] arr){
+    if(arr[0]+arr[2]>=arr[1]+arr[3]){
+        if(arr[0]+arr[2]>=arr[0]+arr[3]){
+            maxrob=arr[0]+arr[2];
+        }
+        else{
+            maxrob=arr[0]+arr[3];
+        }
+    }
+    else if(arr[1]+arr[3]>=arr[0]+arr[2]){
+        if(arr[1]+arr[3]>=arr[0]+arr[3]){
+            maxrob=arr[1]+arr[3];
+        }
+        else{
+            maxrob=arr[0]+arr[3];
+        }   
+    }
+    return maxrob;
+}
+int RoundHouse(int[] arr){
+    if(arr[0]+arr[2]>=arr[1]+arr[3]){
+        maxrob=arr[0]+arr[2];
+    }
+    else{
+        maxrob=arr[1]+arr[3];
+    }
+    
+    return maxrob;
+}
+int SquareHouse(int[] arr){
+    if(arr[0]+arr[2]>=arr[1]+arr[3]){
+        maxrob=arr[0]+arr[2];
+    }
+    else{
+        maxrob=arr[1]+arr[3];
+    }
+    return maxrob;
+}
+int MultiHouse(int[] arr){
+    if(arr[0]+arr[2]+arr[4]>=arr[1]+arr[3]+arr[5]){
+        maxrob=arr[0]+arr[2]+arr[4];
+    }
+    else{
+        maxrob=arr[1]+arr[3]+arr[5];
+    }
+    return maxrob;
+}
+}
+public class MainRobber{
+    public static void main(String args[]){
+        int out=0;
+	int[] arr= new int[6];
+        JAVAProfessionalRobber jpr=new JAVAProfessionalRobber();
+	jpr.MachineLearning(); 
+        System.out.println("Which type of House you want to steal\n1-Row House\n2-Round House\n3-Square House\n4-Multi House\n\nEnter your choice : ");
+        Scanner s = new Scanner(System.in);
+	int choice=s.nextInt();
+        switch(choice){
+            case 1:
+                System.out.println("Enter amount available in 4 houses :\n");
+                for(int i=0;i<4;i++){
+                    arr[i]=s.nextInt();
+                }
+                out=jpr.RowHouse(arr);
+                break;
+            case 2:
+                System.out.println("Enter amount available in 4 houses :\n");
+                for(int i=0;i<4;i++){
+                    arr[i]=s.nextInt();
+                }
+                out=jpr.RoundHouse(arr);
+                break;
+            case 3:
+                System.out.println("Enter amount available in 4 houses :\n");
+                for(int i=0;i<4;i++){
+                    arr[i]=s.nextInt();
+                }
+                out=jpr.SquareHouse(arr);
+                break;
+            case 4:
+                System.out.println("Enter amount available in 6 houses :\n");
+                for(int i=0;i<6;i++){
+                    arr[i]=s.nextInt();
+                }
+                out=jpr.MultiHouse(arr);
+                break;
+            default:
+                System.out.println("Invalid choice");
+                break;
+        }
+    System.out.println("On stealing you can get Max Profit = "+out);
+    }
+}
